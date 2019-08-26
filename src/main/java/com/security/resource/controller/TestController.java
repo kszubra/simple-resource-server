d7a727afd09a977f83a66d481797f57518f13272
Kacper Szubra <kacper.szubra@nvt.pl>
2019-08-26 08:56:10 +0200

    roles work

--- src/main/java/com/security/resource/controller/TestController.java
+++ src/main/java/com/security/resource/controller/TestController.java
@@ -6,17 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    //@Secured("USER")
+
     @RequestMapping("/test")
     public String test() {
-        return "Hello World";
+        return "Hi peasant";
     }
 
-    //@PreAuthorize("#oauth2.clientHasRole('ADMIN')")
-    //@Secured("ADMIN")
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping("/admintest")
     public String adminTest() {
-        return "Admin part";
+        return "Welcome almighty!";
     }
 }
